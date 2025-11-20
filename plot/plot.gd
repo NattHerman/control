81a2f7f2dad064fb562d041f819fd8d95639a2bf
NattHerman <daghermanoyerhamn@gmail.com>
2025-11-20 20:43:14 +0100

    Add types to variable definitions

--- plot/plot.gd
+++ plot/plot.gd
@@ -1,12 +1,12 @@
 extends Control
 class_name Plot
 
-@onready var _background = %Background
-@onready var _center_marker = %Center
+@onready var _background: Panel = %Background
+@onready var _center_marker: Marker2D = %Center
 
-@onready var _data_line = %DataLine
-@onready var x_axis = %XAxis
-@onready var y_axis = %YAxis
+@onready var _data_line: Line2D = %DataLine
+@onready var x_axis: Line2D = %XAxis
+@onready var y_axis: Line2D = %YAxis
 
 
 var plot_origin: Vector2 = Vector2.ZERO
@@ -29,6 +29,7 @@ func _ready() -> void:
 	_center_marker.global_position = _background.get_global_rect().position + plot_size * Vector2(0.1, 1) * 0.5
 	plot_origin = _center_marker.global_position
 	
+	# Set axis positions
 	x_axis.global_position = plot_origin
 	y_axis.global_position = plot_origin
 	x_axis.points[0] = plot_size.x * 0.95 * Vector2(1, 0)
