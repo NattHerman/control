0b47c062cc47f23ccb7c693baf7cde8738eb7537
NattHerman <daghermanoyerhamn@gmail.com>
2025-11-20 20:37:17 +0100

    Create plot

---
+++ plot/plot.gd
@@ -0,0 +1,39 @@
+extends Control
+class_name Plot
+
+@onready var _background = %Background
+@onready var _center_marker = %Center
+
+@onready var _data_line = %DataLine
+@onready var x_axis = %XAxis
+@onready var y_axis = %YAxis
+
+
+var plot_origin: Vector2 = Vector2.ZERO
+var plot_size: Vector2 = Vector2.ZERO
+
+static var data: Array[Vector2]
+
+static func create_plot(_data: Array[Vector2]):
+	data = _data
+	var plot_scene: PackedScene = load("res://plot/plot.tscn")
+	return plot_scene.instantiate()
+
+func _ready() -> void:
+	# Wait untill AFTER the first frame has been drawn
+	await get_tree().process_frame
+	await get_tree().process_frame
+	
+	plot_size = _background.get_global_rect().size
+	
+	_center_marker.global_position = _background.get_global_rect().position + plot_size * Vector2(0.1, 1) * 0.5
+	plot_origin = _center_marker.global_position
+	
+	x_axis.global_position = plot_origin
+	y_axis.global_position = plot_origin
+	x_axis.points[0] = plot_size.x * 0.95 * Vector2(1, 0)
+	x_axis.points[1] = -plot_size.x * 0.05 * Vector2(1, 0)
+	y_axis.points[0] = plot_size.y * 0.5 * Vector2(0, 1)
+	y_axis.points[1] = -plot_size.y * 0.5 * Vector2(0, 1)
+	
+	_data_line.points = PackedVector2Array(data)
