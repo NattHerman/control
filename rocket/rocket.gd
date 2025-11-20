4ea4bbb8367d4b658da73646b126a4ba7a556dfd
NattHerman <daghermanoyerhamn@gmail.com>
2025-11-20 22:58:52 +0100

    rocket.gd is tool, set pid-values at runtime

--- rocket/rocket.gd
+++ rocket/rocket.gd
@@ -1,21 +1,34 @@
+@tool
 extends RigidBody2D
 
-@export_category("Height PID-Controller")
-@export var Kp: float = 0.0
-@export var Ki: float = 0.0
-@export var Kd: float = 0.0
 
 var pid_controller := PIDController.new()
 var reference: float = 1280*3
 
-var drag_coefficient = 0.5 # going off vibes
+var drag_coefficient = 10 # going off vibes
 
 @onready var main_thruster = $MainThruster
+
+@export_category("Height PID-Controller")
+@export var Kp: float = 0.0:
+	set(value):
+		Kp = value
+		pid_controller.Kp = value
+
+@export var Ki: float = 0.0:
+	set(value):
+		Ki = value
+		pid_controller.Ki = value
+
+@export var Kd: float = 0.0:
+	set(value):
+		Kd = value
+		pid_controller.Kd = value
 
 
 func _ready() -> void:
 	pid_controller.set_PID(Kp, Ki, Kd)
-	pid_controller.set_back_propogation(true, 0, main_thruster.max_thrust)
+	pid_controller.set_back_propogation(false, 0, main_thruster.max_thrust)
 	
 	var thrust_Gs: float = 3
 	
@@ -23,19 +36,25 @@ func _ready() -> void:
 
 
 func _physics_process(delta: float) -> void:
-	var height = -position.y
-	var error = reference - height
-	ui_values.error = roundf(error)
-	
-	var u = pid_controller.control(error, delta)
-	
-	main_thruster.set_thrust(u)
-	
-	var drag = linear_velocity.length_squared() * drag_coefficient
-	apply_force(-linear_velocity.normalized() * drag)
-	
-	ui_values.speed = linear_velocity.length()
-	ui_values.positon_list.append(Vector2(
-		Time.get_ticks_msec() * 0.001 * Engine.time_scale,
-		position.y * 0.01
-	))
+	if not Engine.is_editor_hint():
+		var height: float = -position.y
+		var error: float = reference - height
+		ui_values.error = roundf(error)
+		
+		var u_pid: float = pid_controller.control(error, delta)
+		var u_nom: float = 9.81 * mass
+		
+		var u: float = u_pid + u_nom
+		print(round(u_pid), " ", round(u_nom))
+		
+		main_thruster.set_thrust(u)
+		
+		var drag = linear_velocity.length_squared() * drag_coefficient
+		#print(round(linear_velocity.length_squared()),"; " , drag_coefficient)
+		apply_force(-linear_velocity.normalized() * drag)
+		
+		ui_values.speed = linear_velocity.length()
+		ui_values.positon_list.append(Vector2(
+			Time.get_ticks_msec() * 0.001 * Engine.time_scale,
+			position.y * 0.01
+		))
