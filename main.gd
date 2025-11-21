c40a0d0ebc937d53e42a247a18fb55f25470be5e
NattHerman <daghermanoyerhamn@gmail.com>
2025-11-21 20:51:58 +0100

    Engime time scale can be set in editor at runtime

--- main.gd
+++ main.gd
@@ -6,7 +6,12 @@ var reference = 1000
 @onready var rocket = %Rocket
 
 
+@export_range(1, 20, 0.1) var time_scale: float = 1:
+	set(value):
+		time_scale = value
+		Engine.time_scale = time_scale
+
 func _ready() -> void:
-	Engine.time_scale = 1
+	Engine.time_scale = time_scale
 	reference = rocket.reference
 	$ReferenceMarker.position.y = -reference
