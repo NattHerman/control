5d004d7dee70202e745fc218a06714f4e0be0dba
NattHerman <daghermanoyerhamn@gmail.com>
2025-11-23 20:13:06 +0100

    Rocket: reversed sign of reference and height. ready function doesnt run as tool

--- rocket/rocket.gd
+++ rocket/rocket.gd
@@ -3,11 +3,11 @@ extends RigidBody2D
 
 
 var pid_controller := PIDController.new()
-var reference: float = 1280*3
+var reference: float = -1280*3
 
-var drag_coefficient = 10 # going off vibes
-var gravity = 980
-
+const drag_coefficient = 0 # going off vibes
+const gravity = 981
+ 
 @onready var main_thruster = $MainThruster
 
 @export_category("Height PID-Controller")
@@ -28,38 +28,41 @@ var gravity = 980
 
 
 func _ready() -> void:
-	var omega0: float = 2*PI * 1
-	Kp = (omega0 ** 2) * mass
-	Kd = 2 * omega0 * mass
-	
-	pid_controller.set_PID(Kp, Ki, Kd)
-	pid_controller.set_back_propogation(false, 0, main_thruster.max_thrust)
-	
-	var thrust_Gs: float = 3
-	
-	main_thruster.max_thrust = mass * gravity * thrust_Gs
+	if not Engine.is_editor_hint():
+		# Calculate PD values for a critically dampened response.
+		var omega0: float = 2*PI * 0.01
+		Kp = (omega0 ** 2) * mass
+		Kd = 2 * omega0 * mass
+		print("Omega0: ", omega0)
+		print("Kp: ", Kp)
+		print("Kd: ", Kd)
+		
+		pid_controller.set_PID(Kp, Ki, Kd)
+		pid_controller.set_back_propogation(false, 0, main_thruster.max_thrust)
+		
+		var thrust_Gs: float = 3
+		
+		main_thruster.max_thrust = mass * gravity * thrust_Gs
 
 
 func _physics_process(delta: float) -> void:
 	if not Engine.is_editor_hint():
-		var height: float = -position.y
+		var height: float = global_position.y
 		var error: float = reference - height
 		ui_values.error = roundf(error)
 		
 		var u_pid: float = pid_controller.control(error, delta)
-		var u_nom: float = gravity * mass
+		var u_nom: float = -gravity * mass
 		
 		var u: float = u_pid + u_nom
-		print(round(u_pid), " ", round(u_nom))
 		
-		main_thruster.set_thrust(u)
+		main_thruster.set_thrust(-u)
 		
 		var drag = linear_velocity.length_squared() * drag_coefficient
-		#print(round(linear_velocity.length_squared()),"; " , drag_coefficient)
 		apply_force(-linear_velocity.normalized() * drag)
 		
 		ui_values.speed = linear_velocity.length()
 		ui_values.positon_list.append(Vector2(
-			Time.get_ticks_msec() * 0.001 * Engine.time_scale,
-			position.y * 0.01
+			global_timer.time,
+			position.y
 		))
