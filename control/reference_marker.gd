c5c90c62203b8e126864bd027658375fd3fad03e
NattHerman <daghermanoyerhamn@gmail.com>
2025-11-23 20:16:03 +0100

    create reference point marker

---
+++ control/reference_marker.gd
@@ -0,0 +1,7 @@
+extends Marker2D
+
+var reference: Vector2 = Vector2.ZERO:
+	set(value):
+		position = value
+	get:
+		return position
--- main.gd
+++ main.gd
@@ -1,17 +1,31 @@
 extends Node2D
 
 
-var reference = 1000
+var reference:
+	get:
+		return target.reference
+	set(value):
+		target.reference = value
 
-@onready var rocket = %Rocket
+@onready var target = $Drone
 
-
-@export_range(1, 20, 0.1) var time_scale: float = 1:
+@export_range(1, 20, 0.01)
+var time_scale: float = 1:
 	set(value):
 		time_scale = value
 		Engine.time_scale = time_scale
 
+
 func _ready() -> void:
 	Engine.time_scale = time_scale
-	reference = rocket.reference
-	$ReferenceMarker.position.y = -reference
+	if reference is Vector2:
+		$ReferenceLine.position.y = reference.y
+	else:
+		$ReferenceLine.position.y = reference
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.pressed and (event.button_index == MouseButton.MOUSE_BUTTON_LEFT):
+		var centerscreen_mouse_pos = (get_viewport().get_mouse_position()  - get_viewport_rect().size * 0.5)
+		reference = centerscreen_mouse_pos / %Camera2D.zoom + %Camera2D.global_position
+		$ReferenceMarker.reference = reference
+		$ReferenceLine.position.y = reference.y
