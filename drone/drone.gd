03a590b2b602d6942843d9061539d858efc2ed69
NattHerman <daghermanoyerhamn@gmail.com>
2025-11-25 17:30:38 +0100

    Use PID controller function to get critically dampened PD values

--- drone/drone.gd
+++ drone/drone.gd
@@ -1,4 +1,5 @@
 extends RigidBody2D
+## A drone where its x and y positions are regulated using PID-controllers.
 
 
 var y_controller := PIDController.new()
@@ -6,7 +7,7 @@ var x_controller := PIDController.new()
 
 var reference: Vector2 = Vector2(0, -100)
 
-const gravity = 981
+var gravity: float = 981
 
 @onready var left_thruster = %LeftPropeller
 @onready var right_thruster = %RightPropeller
@@ -28,10 +29,7 @@ const gravity = 981
 		return Vector3(x_controller.Kp, x_controller.Ki, x_controller.Kd)
 
 func _ready() -> void:
-	# Calculate PD values for a critically dampened response.
-	var omega0: float = 2*PI * 1
-	y_PID_parameters.x = (omega0 ** 2) * mass
-	y_PID_parameters.z = 2 * omega0 * mass
+	y_controller.PD_critically_dampened(mass)
 	
 	var Gs = 2
 	left_thruster.max_thrust = mass * gravity * Gs
@@ -51,8 +49,9 @@ func _physics_process(delta: float) -> void:
 	left_thruster.set_thrust(-u/2)
 	right_thruster.set_thrust(-u/2)
 	
-	#ui_values.speed = linear_velocity.length()
-	#ui_values.positon_list.append(Vector2(
-		#global_timer.time,
-		#position.y
-	#))
+	ui_values.speed = linear_velocity.length()
+	if Time.get_ticks_msec() % 10 == 0:
+		ui_values.positon_list.append(Vector2(
+			global_timer.time,
+			position.y
+		))
