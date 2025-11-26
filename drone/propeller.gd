6ef9fc3fdd133ade7323e00e4eca0b50eabca5d0
NattHerman <daghermanoyerhamn@gmail.com>
2025-11-26 21:24:00 +0100

    add proper setter for current thrust. draw thrust with using DrawVector

--- drone/propeller.gd
+++ drone/propeller.gd
@@ -2,14 +2,14 @@ extends CollisionShape2D
 
 
 var target_thrust: float = 0 # Force [N]
-var current_thrust: float = 0 # Force [N]
+var current_thrust: float = 0: # Force [N]
+	set(value):
+		current_thrust = clampf(value, -max_thrust, max_thrust)
 
 var max_thrust: float = 0
 
 
 func set_thrust(thrust: float) -> void:
-	#target_thrust = thrust
-	#current_thrust = target_thrust
 	current_thrust = clampf(thrust, -max_thrust, max_thrust)
 
 
@@ -22,3 +22,4 @@ func _process(_delta: float) -> void:
 func _physics_process(_delta: float) -> void:
 	var up = -global_transform.y
 	get_parent().apply_force(up * current_thrust, global_position)
+	DrawVector.new(up * current_thrust * 0.01, global_position, 0.01)
